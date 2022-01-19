ca205c90a9fee27252f1da9e2d67b6855d3771d8
TheBouns <ilbonsi3@hotmail.com>
2022-01-19 12:57:28 +0100

    readme update

--- controller/categoriesController.js
+++ controller/categoriesController.js
@@ -1,43 +1,44 @@
-const db = require ("../config/database");
+const db = require("../config/database");
 
 const categoriesController = {
-    create(req,res){
-        let sql = 'CREATE TABLE categories (id int AUTO_INCREMENT, name_category VARCHAR(45), PRIMARY KEY(id))';
-        db.query(sql,(err,result)=>{
-            if(err)throw err;
-            res.send('Table Has been created');
-        })
-    },
-    add(req,res){
-        let post = { name_category: req.body.name};
-        let sql = 'INSERT INTO categories SET ?'
-        db.query(sql,post,(err,result)=>{
-            if(err)throw err;
-            res.send(`${post.name_category} has added`);
-        })
-    },
-    show(req,res){
-        let sql = 'SELECT * FROM categories';
-        db.query(sql,(err,result)=>{
-            if(err)throw err;
-            res.send(result);
-        })
-    },
-    update(req,res){
-        let newName = req.body.name;
-        let sql = `UPDATE categories SET name_category='${newName}' WHERE id=${req.params.id}`;
-        db.query(sql,(err,result)=>{
-            if(err)throw err;
-            res.send('Completed')
-        })
-    },
-    searchById(req,res){
-        let sql = `SELECT * FROM categories WHERE id=${req.params.id}`;
-        db.query(sql,(err,result)=>{
-            if(err)throw err;
-            res.send(result);
-        })
-    }
-}
+  create(req, res) {
+    let sql =
+      "CREATE TABLE categories (id int AUTO_INCREMENT, name_category VARCHAR(45), PRIMARY KEY(id))";
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send("Table Has been created");
+    });
+  },
+  add(req, res) {
+    let post = { name_category: req.body.name };
+    let sql = "INSERT INTO categories SET ?";
+    db.query(sql, post, (err, result) => {
+      if (err) throw err;
+      res.send(`${post.name_category} has added`);
+    });
+  },
+  show(req, res) {
+    let sql = "SELECT * FROM categories";
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  },
+  update(req, res) {
+    let newName = req.body.name;
+    let sql = `UPDATE categories SET name_category='${newName}' WHERE id=${req.params.id}`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send("Completed");
+    });
+  },
+  searchById(req, res) {
+    let sql = `SELECT * FROM categories WHERE id=${req.params.id}`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  },
+};
 
 module.exports = categoriesController;
--- readme.md
+++ readme.md
@@ -1,42 +1,8 @@
-# Express AND SQL
-
-Exercises to practice endpoints with sql and express.
-
-## What is inside ?
-There is more than 200 lines about endpoints using  CRUD(get,post,put,delete).
-I used diverse endpoint to add Tables to the database,eliminate rows,add rows,etc.
-Server is running in port 64000 just for fun. 
+# Express AND SQL Data Base : 
+This is a simple database developed with Node.js.
+## Geting Started
+- Clone this repository >https://github.com/TheBouns/ExpressSql.git
 ```js
-app.get("/users",(req,res)=>{
-    let sql= 'CREATE TABLE users(id int AUTO_INCREMENT, user_name VARCHAR(45), user_email VARCHAR(45), PRIMARY KEY (id))'
-    db.query(sql, (err,result)=>{
-        if(err)throw err;
-        res.send('Table has been created');
-    })
-})
-app.get("/orders", (req,res)=>{
-    let sql = 'CREATE TABLE orders(id int AUTO_INCREMENT, user_id int,order_numb VARCHAR(45), PRIMARY KEY (id), FOREIGN KEY (user_id)  REFERENCES users(id) ON DELETE CASCADE)'
-    db.query(sql, (err,result)=>{
-        if(err)throw err;
-        res.send('Table has been created');
-    })
-})
-app.post("/users", (req,res)=>{
-    let post= {user_name: req.body.name , user_email : req.body.mail}
-    let sql= 'INSERT INTO users SET ?';
-    db.query(sql,post,(err,result)=>{
-        if(err)throw err;
-        res.send(`${post.user_name} has added`);
-    })
-})
-app.put("/users/:id", (req,res)=>{
-    let newName = req.body.name;
-    let sql = `UPDATE users SET user_name='${newName}' WHERE id=${req.params.id}`;
-    db.query(sql,(err,result)=>{
-        if(err)throw err;
-        res.send('User UPDATED')
-    })
-})
 ````
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
